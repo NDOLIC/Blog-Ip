00d0e3bbdc0dd8ec2b43dd826bf694a0224d8749
NDOLIC <tblaguese1@gmail.com>
2019-03-13 22:46:50 +0200

    all files added

--- app/email.py
+++ app/email.py
@@ -2,9 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-sender_email = 'divinekagame@gmail.com'
 def mail_message(subject,template,to,**kwargs):
-   
+    sender_email = 'biinewtondev@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
--- app/models.py
+++ app/models.py
@@ -1,123 +1,65 @@
 from . import db
-from . import login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
 from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
-
-
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pass_secure = db.Column(db.String(255))
-    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
-    # Flask-Login integration
-    def is_authenticated(self):
-       return True
+    password_hash = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    def is_active(self):
-       return True
-
-    def is_anonymous(self):
-       return False
-
-    def get_id(self):
-       return self.id
-
-   
     @property
     def password(self):
-            raise AttributeError('You cannot read the password attribute')
+        raise AttributeError('You cannot read the password attribute')
 
     @password.setter
     def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+        self.pass_secure = generate_password_hash(password)
 
-
-    def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
 
+
 class Role(db.Model):
-    __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    __tablename__='roles'
 
-    def __repr__(self):
-        return f'User {self.name}'
-class Review(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(255), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-    __tablename__ = 'reviews'
+class Post(db.Model):
 
-    id = db.Column(db.Integer,primary_key = True)
-    movie_id = db.Column(db.Integer)
-    movie_title = db.Column(db.String)
-    image_path = db.Column(db.String)
-    movie_review = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    __tablename__ = 'posts'
 
-    def save_review(self):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def save_post(self):
+        '''
+        Function to save a new blog.
+        '''
         db.session.add(self)
         db.session.commit()
-
-    @classmethod
-    def get_reviews(cls,id):
-        reviews = Review.query.filter_by(movie_id=id).all()
-        return reviews
