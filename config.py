9935a0cb7a2e0bcb9724dae315ad3dcbbed37203
NDOLIC <tblaguese1@gmail.com>
2019-03-14 14:43:26 +0200

    adding nav bar

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
-class Config:
-
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://divine:dDivine123@@localhost/watchlist'
+class Config():
+    '''
+    parent class config
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bknngeno:123@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -13,20 +12,29 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
+    SECRET_KEY=os.environ.get('SECRET_KEY') 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
-
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://divine:dDivine123@@localhost/watchlist'
-    
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bknngeno:123@localhost/blog'
     DEBUG = True
 
+
+class TestConfig(Config):
+    pass
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'production':ProdConfig,
+    'development':DevConfig,
+    'test':TestConfig
 }
