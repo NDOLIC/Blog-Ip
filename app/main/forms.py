24e0d2673f284a79022a849f70931e30207c5727
NDOLIC <tblaguese1@gmail.com>
2019-03-14 09:25:48 +0200

    adding form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,21 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,TextAreaField,SubmitField
+# from wtforms.validators import Required
+
+# class ReviewForm(FlaskForm):
+
+#     title = StringField('Review title',validators=[Required()])
+#     review = TextAreaField('Movie review', validators=[Required()])
+#     submit = SubmitField('Submit')
+# class UpdateProfile(FlaskForm):
+#     bio = TextAreaField('Tell us about you.',validators = [Required()])
+#     submit = SubmitField('Submit')
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User, Post
+from wtforms import ValidationError
 
-class ReviewForm(FlaskForm):
-
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+class PostForm(FlaskForm):
+    body = TextAreaField(validators=[Required()])
     submit = SubmitField('Submit')
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
-   
--- app/main/views.py
+++ app/main/views.py
@@ -1,124 +1,43 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..request import get_movies,get_movie,search_movie
-from .forms import ReviewForm,UpdateProfile
+from ..models import User, Post
+from .forms import PostForm
 from .. import db
-from ..models import Review, User
-from flask_login import login_required,current_user
-from .. import db,photos
+from flask_login import login_user, logout_user, login_required, current_user
+# from ..email import mail_message
 
 
-# Views
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     message = 'Hello World'
-#     return render_template('index.html',message = message)
 @main.route('/')
 def index():
+    """View root page function that returns index page and the various news sources"""
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+    title = 'Home- Welcome to app of blog'
 
-    # Getting popular movie
-    popular_movies = get_movies('popular')
-    upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
+    form = PostForm()
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
-
-    search_movie = request.args.get('movie_query')
-
-    if search_movie:
-        return redirect(url_for('main.search',movie_name=search_movie))
-    else:
-        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-@main.route('/movies/<int:id>')
-def movies(movie_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
-@main.route('/movie/<int:id>')
-def movie(id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    movie = get_movie(id)
-    title = f'{movie.title}'
-    reviews = Review.get_reviews(movie.id)
-
-    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-@main.route('/search/<movie_name>')
-def search(movie_name):
-    '''
-    View function to display the search results
-    '''
-    movie_name_list = movie_name.split(" ")
-    movie_name_format = "+".join(movie_name_list)
-    searched_movies = search_movie(movie_name_format)
-    title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies)
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_review(id):
-    form = ReviewForm()
-    movie = get_movie(id)
     if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
+        post = Post(body=form.body.data, author=current_user._get_current_object())
+        post.save_post()
+        return redirect(url_for('.index'))
 
-        # Updated review instance
-        new_review = Review(movie_id=movie.id,movie_title=title,image_path=movie.poster,movie_review=review,user=current_user)
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
 
-        # save review method
-        new_review.save_review()
-        return redirect(url_for('.movie',id = movie.id ))
+    return render_template('index.html', form=form, posts=posts)
 
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(username = uname).first()
 
+@main.route('/user/<username>')
+@login_required
+def user(username):
+    """View function that returns the homepage for a particular user when they sign in"""
+    user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
-@login_required
-def update_profile(uname):
-    user = User.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
 
-    form = UpdateProfile()
-
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('.profile',uname=user.username))
-
-    return render_template('profile/update.html',form =form)    
-
-@main.route('/user/<uname>/update/pic',methods= ['POST'])
-@login_required
-def update_pic(uname):
-    user = User.query.filter_by(username = uname).first()
-    if 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        path = f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('posts.html', posts=[post])
