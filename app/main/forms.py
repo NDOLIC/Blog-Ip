1ec3780a44cd47dca61217653e31341d4bd9cee7
NDOLIC <tblaguese1@gmail.com>
2019-03-14 10:44:38 +0200

    adding comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,3 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,TextAreaField,SubmitField
-# from wtforms.validators import Required
-
-# class ReviewForm(FlaskForm):
-
-#     title = StringField('Review title',validators=[Required()])
-#     review = TextAreaField('Movie review', validators=[Required()])
-#     submit = SubmitField('Submit')
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('Tell us about you.',validators = [Required()])
-#     submit = SubmitField('Submit')
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import Required, Email, EqualTo
